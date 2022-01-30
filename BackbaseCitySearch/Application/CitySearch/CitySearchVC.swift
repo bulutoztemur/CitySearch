4031d4ea59d656e4d88dd7358bc899b30da39556
ALAATTIO <alaattinbulut.oztemur@intertech.com.tr>
2022-01-30 13:44:11 +0300

    Project folder hierarchy structured

---
+++ BackbaseCitySearch/Application/CitySearch/CitySearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  CitySearchVC.swift
+//  BackbaseCitySearch
+//
+//  Created by alaattinbulut on 30.01.2022.
+//
+
+import UIKit
+
+class CitySearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .magenta
+
+    }
+}
---
+++ BackbaseCitySearch/Application/CitySearch/CitySearchVM.swift
@@ -0,0 +1,12 @@
+//
+//  CitySearchVM.swift
+//  BackbaseCitySearch
+//
+//  Created by alaattinbulut on 30.01.2022.
+//
+
+import Foundation
+
+final class CitySearchVM {
+    
+}
